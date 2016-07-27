693b3af0d71ac03346a374982c963ab1027996a6
Sash SashshOK <sashshok@gmail.com>
2016-07-27 15:21:01 +0300

    Add minimal html

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-import lists
+import lists.views
 
 # urlpatterns = [
 #     url(r'^admin/', include(admin.site.urls)),
