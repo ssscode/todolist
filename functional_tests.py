f7e8825f411d471f7faea182da00540ad663bc7e
Sash SashshOK <sashshok@gmail.com>
2016-07-27 21:13:46 +0300

    Refactor add new func for check list item to FT

--- functional_tests.py
+++ functional_tests.py
@@ -18,6 +18,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
     
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        
+        self.assertIn(row_text, [row.text for row in rows])
+    
     def test_can_start_a_list_and_retrieve_it_later(self):
         # check homepage for superlists
         self.browser.get('http://localhost:8000')
@@ -35,25 +41,15 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy peacock feathers')
         # expect after press enter, page will reload and new item "Buy peacock feathers" available in the list
         inputbox.send_keys(Keys.ENTER)
-        
-        # import time
-        # time.sleep(10)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(
-        #     any(row.text == '1: Buy peacock feathers' for row in rows),
-        #     "New to-do item did not appear in table -- its text was:\n%s" % (table.text,
-        #     )
-        # )
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         
         # text box invite add new item
-        self.fail('Finish the test!')
         # add new item "Use peacock feathers to make a fly"
-        
+        inputbox = self.browser.find_elements_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(keys.ENTER)
         # The page updates again, and now shows both items on her list
-
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         # Edith wonders whether the site will remember her list. Then she sees
 
         # that the site has generated a unique URL for her -- there is some
@@ -63,6 +59,7 @@ class NewVisitorTest(unittest.TestCase):
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
+        self.fail('Finish the test!')
 
 
 if __name__ == '__main__':
