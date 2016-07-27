77a932e8be4416c83aba759aba4241e5bdad37d4
Sash SashshOK <sashshok@gmail.com>
2016-07-27 21:20:49 +0300

    Fix FT

--- functional_tests.py
+++ functional_tests.py
@@ -45,10 +45,11 @@ class NewVisitorTest(unittest.TestCase):
         
         # text box invite add new item
         # add new item "Use peacock feathers to make a fly"
-        inputbox = self.browser.find_elements_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
-        inputbox.send_keys(keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
         # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         # Edith wonders whether the site will remember her list. Then she sees
 
