164acd656ffbf6c6b1e600d595f6653a081a5960
Sash SashshOK <sashshok@gmail.com>
2016-07-29 15:27:49 +0300

    Split urls for lists

--- lists/urls.py
+++ lists/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 import lists.views
-import lists.urls
 
 # urlpatterns = [
 #     url(r'^admin/', include(admin.site.urls)),
 # ]
 
 urlpatterns = patterns('',
-    url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/', include(lists.urls)),
+    url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
+    url(r'^new$', 'lists.views.new_list', name='new_list'),
 )
