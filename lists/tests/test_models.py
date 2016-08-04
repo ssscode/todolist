ffe44801135e1d5e68a5f12ea26ac112bc45ca00
Sash SashshOK <sashshok@gmail.com>
2016-08-04 15:01:26 +0300

    Add new form for Lists

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -42,3 +42,4 @@ class ListAndItemModelTest(TestCase):
     def test_get_absolute_url(self):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
+    
